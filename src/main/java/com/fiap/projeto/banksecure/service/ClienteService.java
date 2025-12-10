f61e3c8bb5257036a40c0c22af5b971bf3e55181
suino <sampaiov917@gmail.com>
2025-12-09 21:27:25 -0300

    Adicionando import do java.time.LocalDate, adicionando Métodos de cadastro e de listagem de clientes e adicionando campo coberturaMinima em Seguro

--- src/main/java/com/fiap/projeto/banksecure/service/ClienteService.java
+++ src/main/java/com/fiap/projeto/banksecure/service/ClienteService.java
@@ -1,10 +1,17 @@
 package com.fiap.projeto.banksecure.service;
 
 import com.fiap.projeto.banksecure.domain.Cliente;
+import com.fiap.projeto.banksecure.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
 
     public void validarCadastro(Cliente cliente) {
         if (cliente == null) {
@@ -30,5 +37,14 @@ public class ClienteService {
         if (cliente.getTelefone() == null || cliente.getTelefone().trim().isEmpty()) {
             throw new IllegalArgumentException("Telefone do cliente é obrigatório.");
         }
+    }
+
+    public Cliente cadastrarCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarClientes(){
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes;
     }
 }
